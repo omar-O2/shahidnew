0e6f367361fd1e9ff72193aecb6e7a73b7e92de3
o.radwan <o.radwan@tammwel.com>
2025-05-14 22:55:09 +0300

    sa

--- src/test/java/Profile_Test.java
+++ src/test/java/Profile_Test.java
@@ -21,6 +21,22 @@ public class Profile_Test extends Initiate_Shahid{
                 false,
                 "No EditText elements found");
     }
+
+    @Test
+    public void Edit_Profile() throws InterruptedException {
+        new Sign_In_Page(driver).Press_On_sign().Enter_Valid_Email().Enter_Password().Press_On_Sign_In_button();
+        new Profile_Page(driver).Press_Choose_Profile_Icon().Press_Profile_Icon().Press_Profile_Management_Button().Press_Edit_Icon();
+        new Registration_Page(driver).Press_On_Ok_Button();
+        new Profile_Page(driver).Press_Delete_Button().Press_Confirm_Delete_Button();
+
+
+        Thread.sleep(2000);
+        Assert.assertEquals(
+
+                driver.findElements(By.cssSelector("div[class='toast-notification'] span")).isEmpty(),
+                false,
+                "No EditText elements found");
+    }
     @Test
     public void Delete_Profile() throws InterruptedException {
         new Sign_In_Page(driver).Press_On_sign().Enter_Valid_Email().Enter_Password().Press_On_Sign_In_button();
